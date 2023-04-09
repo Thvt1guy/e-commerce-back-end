acd24001ff102fdf53b6d9b32b667eb06d78457e
Derrick Alcius <derrickalcius@gmail.com>
2023-04-09 12:44:27 -0400

    All Routes properly formatted to CRUD specifications. Works within Insomnia

--- Develop/routes/api/product-routes.js
+++ Develop/routes/api/product-routes.js
@@ -130,7 +130,7 @@ router.delete('/:id', async (req, res) => {
     res.status(404).json({ message: 'No product found with this id!' });
     return;
   }
-  res.status(200).json(locationData);
+  res.status(200).json(productData);
 } catch (err) {
   res.status(500).json(err);
 }
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -7,10 +7,10 @@ router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
   try {
-    const productData = await Tag.findAll({
-      include: [{model: Product}]
+    const tagData = await Tag.findAll({
+      include: [{all: true}]
     });
-    res.status(200).json(productData);
+    res.status(200).json(tagData);
   } catch (err) {
     res.status(500).json(err);
   }
@@ -21,11 +21,11 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Product data
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{model: Product}]
+      include: [{all: true}]
     });
 
     if(!tagData){
-      res.status(404).json({message: "No product found with this id!"});
+      res.status(404).json({message: "No tag found with this id!"});
       return;
     }
 
@@ -45,12 +45,41 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  try { 
+    const tagData = await Tag.update(req.body, {
+    where: {
+      id: req.params.id,
+    },
+  });
+  if(!tagData){
+    res.status(404).json({message: "Tag not Found!"});
+    return;
+  }
+  res.status(200).json(tagData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  try {
+    const tagData = await Tag.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+    if (!tagData) {
+      res.status(404).json({ message: 'No tag found with this id!' });
+      return;
+    }
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
